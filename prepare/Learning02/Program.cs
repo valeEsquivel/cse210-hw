5ff1d4e97984667c3b7397b01d8df45f1ac83e4a
Valeria Esquivel <valeria.esquivel@grupocassa.com>
2023-11-06 23:17:06 -0600

    Learning Act Week 02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Red";
+        job1._jobTitle = "Trainee";
+        job1._startYear = 2019;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._company = "Grupo CASSA";
+        job2._jobTitle = "Sofware Developer";
+        job2._startYear = 2020;
+        job2._endYear = 2023;
+
+        Resume resume = new Resume();
+        resume._name = "Valeria Esquivel";
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.DisplayResume();
     }
 }
